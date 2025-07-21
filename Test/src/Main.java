acc6a3b18dc7bce5d159705068eec2f7e3c67e4c
ArtsiomKulhin <artemkulgin147@gmail.com>
2025-07-21 16:45:39 +0300

    update

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -5,18 +5,11 @@ public class Main {
     public static void main(String[] args) {
           Scanner in = new Scanner(System.in);
           PrintStream out = new PrintStream(System.out);
-          Animal animal = new Dog();
-          animal.info();
+          lambda<Integer> operation;
+          operation = (x,y)->x+y;
+          out.print(operation.calculate(100,450));
     }
 }
-class Animal {
-    public void info() {
-        System.out.print("Я родитель класса Dog");
-    }
-}
-class Dog extends Animal {
-    @Override
-    public void info(){
-        System.out.print("Я наследник класса Animal");
-    }
+interface lambda<T> {
+    T calculate(T x, T y);
 }
